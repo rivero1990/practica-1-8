b73381ad00e47b9d43addb0e29d7fa7a3d9e1065
Anibal Rivero <riveroleonel@gmail.com>
2023-04-17 21:52:27 -0300

    operadores de condicion

---
+++ operadores.js
@@ -0,0 +1,51 @@
+let operador1 = (4 < 4);
+console.log(operador1);   
+
+let operador2 = (23 == 33);
+console.log(operador2); 
+
+let operador3 = (37 >= -3);
+console.log(operador3); 
+
+let operador4 = ("Joel" == "Joel");
+console.log(operador4); 
+
+let operador5 = ("joel" == "Joel");
+console.log(operador5); 
+
+let operador6 = ((`$!;14` != "123qwerty"));
+console.log(operador6);
+
+let operador7 = (("Joel" == "Joel")) && ((`!$:;14` == "123qwerty"));
+console.log(operador7); 
+
+let operador8 = (("Joel" == "joel") || (`!"$:;14`) == (`!"$:;14`));
+console.log(operador8); 
+
+let operador9 = (("Joel" != "Joel") && (`!"$:;14` != "123qwerty"));
+console.log(operador9);
+
+let operador10 = (("joel" != "Joel") && (`!"$:;14` != "123qwerty"));
+console.log(operador10);
+
+let operador11 = (!(("joel" != "Joel") && (`!"$:;14` != "123qwerty")));
+console.log(operador11);
+
+let operador12 = ((23 >= 18) && (23 < 100) );
+console.log(operador12);
+
+let operador13 = ((35 >= 18) || (35 < 100));
+console.log(operador13);
+
+let operador14 = ((10 <= 18) || (10 < 100));
+console.log(operador14);
+
+let operador15 = ((56 < 18) && (56 > 100));
+console.log(operador15);
+
+let operador16 = ((11 < 18) || (11 > 100));
+console.log(operador16);
+
+
+
+
